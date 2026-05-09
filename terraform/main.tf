8f9f120c4d4e83c03dc134fdbd09aade0a9486a3
Dhia Ben Saidane <dhia.bensaidane@SOTUPA.LOCAL>
2026-05-09 11:54:31 +0100

    Hardcode AZs for subnets

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,7 +38,7 @@ resource "aws_subnet" "public" {
   count                   = 2
   vpc_id                  = aws_vpc.main.id
   cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  availability_zone       = ["us-east-1a", "us-east-1b"][count.index]
   map_public_ip_on_launch = true
 
   tags = {
